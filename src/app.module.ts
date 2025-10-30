18c389bc98dfe76d4f16a0a19c7944528dafa4cb
CodeSandbox <support@codesandbox.io>
2025-10-30 20:27:23 +0000

    Sandbox snapshot commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { sequelizeModule } from './sequelize/sequelizeModule';
 
 @Module({
-  imports: [],
+  imports: [sequelizeModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/sequelize/models/user.model.ts
@@ -0,0 +1,57 @@
+import { Optional } from 'sequelize';
+import {
+  AllowNull,
+  Column,
+  PrimaryKey,
+  Model,
+  Table,
+  IsEmail,
+  Default,
+  DataType,
+} from 'sequelize-typescript';
+
+interface UserAttributes {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  isActive: boolean;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+@Table
+export default class User extends Model<
+  UserAttributes,
+  UserCreationAttributes
+> {
+  @Default(DataType.UUIDV4)
+  @Column({
+    type: DataType.UUID,
+  })
+  @PrimaryKey
+  id!: string;
+
+  @AllowNull(false)
+  @Column
+  firstName!: string;
+
+  @AllowNull(false)
+  @Column
+  lastName!: string;
+
+  @AllowNull(false)
+  @IsEmail
+  @Column
+  email!: string;
+
+  @AllowNull(false)
+  @Column
+  password!: string;
+
+  @AllowNull(false)
+  @Default(true)
+  @Column
+  isActive: boolean;
+}
