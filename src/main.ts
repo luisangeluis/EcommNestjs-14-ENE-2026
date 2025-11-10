45c250ceb71bac795bf6f13785c024eaf2b17ffc
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2025-11-10 19:54:00 +0000

    Feature:Added swagger dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Sequelize } from 'sequelize-typescript';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //DTO's
   app.useGlobalPipes(
     new ValidationPipe({
       // strips out properties not in the DTO
@@ -13,10 +15,20 @@ async function bootstrap() {
     }),
   );
 
+  //SEQUELIZE
   const sequelize = app.get(Sequelize);
-
   const isDev = process.env.NODE_ENV !== 'production';
   await sequelize.sync({ force: isDev });
+
+  //SWAGGER
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce Nest')
+    .setDescription('Ecommerce with Nestjs')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(3000);
 }
